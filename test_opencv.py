742be3e9bbb067f5646ae040eb65cdfca85f2606
ragnar17 <edwardamit17@gmail.com>
2021-03-24 21:39:24 +0530

    modified

--- test_opencv.py
+++ test_opencv.py
@@ -6,24 +6,27 @@ path = 'images/a.png'
 # path = 'images/1000px-image.jpg'
 img = cv2.imread(path,1)
 c = np.copy(img)
-pr,pb = paillier.generateKeypair(15)
+pr,pb = paillier.generateKeypair(8)
+print(pb.n2)
 bb = []
 mod = 256
+print(paillier.decrypt(pr,pb,paillier.encrypt(pb,10)))
 for i in range(len(c)):
 	bb.append([0]*len(c[i]))
 	for j in range(len(c[i])):
 		bb[i][j] = [0]*3
 		for k in range(3):
 			E = paillier.encrypt(pb,int(c[i][j][k]))
-			bb[i][j][k] = E//mod
-			c[i][j][k] = E%mod
+			# bb[i][j][k] = E//mod
+			c[i][j][k] = E
 cv2.imshow('Encrypted',c)
 m = np.copy(c)
 bc = 5
 for i in range(len(c)):
 	for j in range(len(c[i])):
 		for k in range(3):
-			m[i][j][k] = paillier.decrypt(pr,pb,int(bb[i][j][k]*mod)+int(c[i][j][k]))
+			m[i][j][k] = paillier.decrypt(pr,pb,int(c[i][j][k]))
+			# m[i][j][k] = paillier.decrypt(pr,pb,int(bb[i][j][k]*mod)+int(c[i][j][k]))
 
 cv2.imshow('Decrypted',m)
 cv2.waitKey(0)
