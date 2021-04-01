b0df556d3d839d005c104204facda5f88066086f
ragnar17 <edwardamit17@gmail.com>
2021-04-01 22:18:34 +0530

    removed class member varibale pb

--- floating_point.py
+++ floating_point.py
@@ -1,12 +1,11 @@
 import paillier
 
 class FloatingPoint:
-	def __init__(self, mantissa, exponent, pb):
+	def __init__(self, mantissa, exponent):
 		self.mantissa = mantissa
 		self.exponent = exponent
-		self.pb = pb
-	# def __repr__(self):
-	# 	return "Public Key : (%s, %s)" %(self.n,self.g)
+	def __repr__(self):
+		return "(%s, %s)" %(self.mantissa,self.exponent)
 
 def encryptFP(pb, x):
 	"""
@@ -19,7 +18,7 @@ def encryptFP(pb, x):
 	while x % 10 == 0:
 		x = x // 10
 		e += 1
-	return FloatingPoint(paillier.encrypt(pb, x), e, pb)
+	return FloatingPoint(paillier.encrypt(pb, x), e)
 
 def getValue(pr, pb, x):
 	"""
@@ -33,7 +32,7 @@ def getValue(pr, pb, x):
 			ret = ret / 10
 	return ret
 
-def addEncEnc(x, y):
+def addEncEnc(pb,x, y):
 	"""
 	Adds 2 FP numbers x, y in their encryptred form & returns the result
 	in encrypted form as well.
@@ -43,15 +42,15 @@ def addEncEnc(x, y):
 	res_exponenet = min(y.exponent, x.exponent)
 
 	if x.exponent < y.exponent:
-		mantissa_y = paillier.homomorphicMul(x.pb, mantissa_y, 10 ** (y.exponent - x.exponent))
+		mantissa_y = paillier.homomorphicMul(pb,mantissa_y, 10 ** (y.exponent - x.exponent))
 	else:
-		mantissa_x = paillier.homomorphicMul(x.pb, mantissa_x, 10 ** (x.exponent - y.exponent))
+		mantissa_x = paillier.homomorphicMul(pb,mantissa_x, 10 ** (x.exponent - y.exponent))
 
-	res_mantissa = paillier.homomorphicAddC(x.pb, mantissa_x, mantissa_y)
+	res_mantissa = paillier.homomorphicAddC(pb,mantissa_x, mantissa_y)
 
-	return FloatingPoint(res_mantissa, res_exponenet, x.pb)
+	return FloatingPoint(res_mantissa, res_exponenet)
 
-def multiplyEncPlain(x, y):
+def multiplyEncPlain(pb,x, y):
 	"""
 	Multiplies encrypted x with plain text FP y 
 	"""
@@ -63,17 +62,18 @@ def multiplyEncPlain(x, y):
 		y = y // 10
 		e += 1
 
-	return FloatingPoint(paillier.homomorphicMul(x.pb, x.mantissa, y) , x.exponent + e, x.pb)
+	return FloatingPoint(paillier.homomorphicMul(pb, x.mantissa, y) , x.exponent + e)
+
+# def subtractEncPlain(pbx,y):
 
 
 
+pr,pb = paillier.generateKeypair(9)
 
-# pr,pb = paillier.generateKeypair(9)
-
-# x = encryptFP(pb, 2.4)
-# y = encryptFP(pb, .23213)
+x = encryptFP(pb, 5)
+y = encryptFP(pb, 2)
 
 # print(getValue(pr, pb, x))
 # print(getValue(pr, pb, y))
-
-# print(getValue(pr, pb, multiplyEncPlain(x, 3.44)))
+# print(getValue(pr,pb,addEncEnc(pb,x,y)))
+# print(getValue(pr, pb, multiplyEncPlain(pb,x, 3)))
